e42362f0b0450f64e469c718f5d0cd0d50d649e6
Aur Saraf <SonOfLilit@gmail.com>
2010-04-27 22:45:39 +0300

    year: solution for T>2 - should work, not well tested

--- year_of_more_code_jam/year.hs
+++ year_of_more_code_jam/year.hs
@@ -27,10 +27,12 @@ tests = runTestTT $ test [
     occurences 1 [[0, 1, 2, 3, 4]] ~?= [1],
     meetingOfK 100 [[0, 3, 7], []] 1 ~?= (meetingOfK 100 [[0, 3, 7]] 1) * 100,
     occurences 100 [[0, 3, 7], [0, 2, 6]] ~?= occurences 100 [[0, 2, 6], [0, 3, 7]],
-    (solve $ Case 4 [[0,1,3], [0, 2]]) ~?= Solution (41%8)]
+    (solve $ Case 4 [[0,1,3], [0, 2]]) ~?= Solution (41%8),
+    occurences 1 [[0], [0], [0]] ~?= [3, 3, 1],
+    (solve $ Case 1 [[0], [0], [0]]) ~?= Solution (9%1)]
 
 solve :: Case -> Solution
-solve (Case year schedules) = Solution $ occurencesToHapiness year (occurences year schedules)
+solve (Case year schedules) = Solution $ occurencesToHapiness year $ correctOccurences (occurences year schedules)
 
 occurences :: Integer -> [Schedule] -> [Integer]
 occurences year schedules = map (meetingOfK year schedules) [1..toInteger $ length schedules]
@@ -46,20 +48,32 @@ meetingOfK year schedules k = spaceFactor * occurencesInLenSchedDimensions
               else
                   sum $ map (\s -> meetingOfK year s k) (kplets k schedules)
 
-cartesian :: [[Integer]] -> [[Integer]]
+cartesian :: [[a]] -> [[a]]
 cartesian [a] = [[x] | x <- a]
 cartesian [a, b] = [[x, y] | x<-a, y<-b]
+cartesian [a, b, c] = [[x, y, z] | x<-a, y<-b, z<-c]
 
-kplets 1 [a,b] = [[a], [b]]
+kplets :: Integer -> [a] -> [[a]]
+kplets 1 list = cartesian [list]
+kplets 2 [a, b, c] = [[a, b], [a, c], [b, c]]
 
 occurence year offsets = max 0 (year - maximum offsets)
 
+correctOccurences :: [Integer] -> [Integer]
+correctOccurences list = correct list 1
+
+correct :: [Integer] -> Integer -> [Integer]
+correct last@[_] _ = last
+correct (this : rest) k = (this - sum (zipWith (*) ks corrected) : corrected)
+    where ks = [k+1..]
+          corrected :: [Integer]
+          corrected = correct rest (k+1)
+
 occurencesToHapiness :: Integer -> [Integer] -> Rational
-occurencesToHapiness year [one] = one % year
-occurencesToHapiness year [a, b] = ((one - 2*two)*(1^2) + two*(2^2)) / y^2
-    where one = toRational a
-          two = toRational b
-          y = toRational year
+occurencesToHapiness year list = (sum scores) % (year^listLen)
+    where scores = zipWith (*) list squares
+          squares = (map (^2) [1..])
+          listLen = toInteger $ length list
 
 formatSolution (Solution r) = show x ++ "+" ++ show y ++ "/" ++ show z
     where a = numerator r
